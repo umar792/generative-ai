00a2dab129c0690b2b5e250ccfa8143c8f580ded
Umar-silicon <umar@sphericesolutions.com>
2025-09-24 20:05:14 +0500

    complete chain almost

--- phase02/LangChain/chain.py
+++ phase02/LangChain/chain.py
@@ -1,7 +1,7 @@
 from langchain_groq import ChatGroq
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnableParallel
+from langchain_core.runnables import RunnableParallel , RunnableBranch , RunnableLambda
 from dotenv import load_dotenv
 import os
 
@@ -70,35 +70,70 @@ model = ChatGroq(
 
 # ---------------------------------- parallel chain------------------------------------
 
-prompt1 = PromptTemplate(
-    template='Generate a short note on given text \n text=> {text}',
-    input_variables=['text']
-)
-prompt2 = PromptTemplate(
-    template='Generate a 5 quiz on given text \n text => {text}',
-    input_variables=['text']
-)
-prompt3 = PromptTemplate(
-    template='Merge the given note and quiz \n notes=> {note} \n quiz => {quiz}',
-    input_variables=['note','quiz']
-)
+# prompt1 = PromptTemplate(
+#     template='Generate a short note on given text \n text=> {text}',
+#     input_variables=['text']
+# )
+# prompt2 = PromptTemplate(
+#     template='Generate a 5 quiz on given text \n text => {text}',
+#     input_variables=['text']
+# )
+# prompt3 = PromptTemplate(
+#     template='Merge the given note and quiz \n notes=> {note} \n quiz => {quiz}',
+#     input_variables=['note','quiz']
+# )
+
+# parser = StrOutputParser()
+
+# parallel_chin = RunnableParallel({
+#     'note' : prompt1 | model | parser,
+#     "quiz" : prompt2 | model | parser
+# })
+
+# merge_chain = prompt3 | model | parser
+
+# chain = parallel_chin | merge_chain
+
+# text = 'your_text'
+
+# result = chain.invoke({"text" : text})
+# print(result)
+
+# chain.get_graph().print_ascii()
+
+# --------------------------------------------------------------------------------------
+
+
+# --------------------------------------- conditional Chain ---------------------------
 
 parser = StrOutputParser()
 
-parallel_chin = RunnableParallel({
-    'note' : prompt1 | model | parser,
-    "quiz" : prompt2 | model | parser
-})
 
-merge_chain = prompt3 | model | parser
+prompt1 = PromptTemplate(
+    template='Give the user sentiment feedback of the user in positive or negative, just give me answer in Negative or Positive \n feedback=> {feedback},',
+    input_variables=['feedback'],
+)
 
-chain = parallel_chin | merge_chain
+prompt2 = PromptTemplate(
+    template='Provide a brief reply to the positive feedback \n feedback=> {feedback},',
+    input_variables=['feedback'],
+)
 
-text = 'your_text'
+prompt3 = PromptTemplate(
+    template='Provide a brief reply to the negative feedback \n feedback=> {feedback},',
+    input_variables=['feedback'],
+)
 
-result = chain.invoke({"text" : text})
+sentiment_chain = prompt1 | model | parser
+
+branch_chain = RunnableBranch(
+    (lambda x:x == "Positive", prompt2 | model | parser ),
+    (lambda x:x == "Negative", prompt3 | model | parser ),
+    RunnableLambda(lambda x: 'could not found sentiment')
+)
+
+chain = sentiment_chain | branch_chain
+
+result = chain.invoke({"feedback": "This product is good"})
 print(result)
-
 chain.get_graph().print_ascii()
-
-# --------------------------------------------------------------------------------------
